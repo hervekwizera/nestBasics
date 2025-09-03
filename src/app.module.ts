0fba23903141f2a1bc1da0fa21000b14871c353e
hervekwizera <hervekwizera63@gmail.com>
2025-09-03 21:36:49 +0200

    creating movies controller,service and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BookController } from './app.controller';
 import { BooksService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
-  controllers: [BookController],
-  providers: [BooksService],
+  imports: [MoviesModule],
+  controllers: [BookController, MoviesController],
+  providers: [BooksService, MoviesService],
 })
 export class AppModule {}
